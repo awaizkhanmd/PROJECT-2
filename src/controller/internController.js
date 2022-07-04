0594663a5845ddac212e7ce1f964a45d9ec2901c
awaizkhanmd <awaizkhanmd@gmail.com>
2022-07-04 18:20:25 +0530

    100%

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const InternModel = require("../models/internModel");
 const CollegeModel = require('../models/collegeModel')
 
@@ -15,16 +14,19 @@ const isValid = function (value) {
 const createInterns = async function (req, res) {
     try {
         let data = req.body;
-        
-        if (Object.keys(data).length==0) return res.status(400).send({ status: false, message: "Body must be present." })
 
-        if (!isValid(data.name)) return res.status(400).send({ status: false, message: "name is required." })
+        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "Body must be present." })
 
-        if (!data.name) return res.status(400).send({ status: false, message: "name must be present" })
 
-        if (!data.email) return res.status(400).send({ status: false, message: "email must be present" })
+        let {name,email,mobile,collegeName} = data
 
-        const validEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(data.email)
+        if (!isValid(name)) return res.status(400).send({ status: false, message: "name is required." })
+
+        if (!name) return res.status(400).send({ status: false, message: "name must be present" })
+
+        if (!email) return res.status(400).send({ status: false, message: "email must be present" })
+
+        const validEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email)
 
         if (!validEmail) return res.status(400).send({ status: false, message: "email is not valid." })
 
@@ -32,29 +34,33 @@ const createInterns = async function (req, res) {
 
         if (findEmail) return res.status(400).send({ status: false, message: "email is already present." })
 
-        
-        if (!data.mobile) return res.status(400).send({ status: false, message: "mobile must be present" })
 
-        const validMobile = /^(\+\d{1,3}[- ]?)?\d{10}$/.test(data.mobile)
+        if (!mobile) return res.status(400).send({ status: false, message: "mobile must be present" })
+
+        const validMobile = /^(\+\d{1,3}[- ]?)?\d{10}$/.test(mobile)
 
         if (!validMobile) return res.status(400).send({ status: false, message: "mobile number is invalid." })
 
-        const uniqueMobile = await InternModel.findOne({ mobile: data.mobile })
+        const uniqueMobile = await InternModel.findOne({ mobile:mobile })
 
         if (uniqueMobile) return res.status(400).send({ status: false, message: "mobile number already exists." })
 
-          if(typeof(data.mobile) === "string") return res.status(400).send({ status: false, message: "mobile number should be in numbers only." })
+        if (typeof (data.mobile) === "string") return res.status(400).send({ status: false, message: "mobile number should be in numbers only." })
 
-        if (!data.collegeId) return res.status(400).send({ status: false, message: "collegeId must be present" })
-        const validCollegeId = await CollegeModel.findById({_id :data.collegeId})
+        if (!collegeName) return res.status(400).send({ status: false, message: "college Name must be present" })
 
-        if(!validCollegeId) return res.status(404).send({ status: false, message: "collegeId is invalid." })
+        if (!isValid(collegeName)) return res.status(400).send({ status: false, message: "college Name is Invalid" })
 
-        if (!mongoose.isValidObjectId(data.collegeId)) return res.status(400).send({ status: false, message: "objectId must be present" })
+        const CollegeData = await CollegeModel.findOne({name:data.collegeName})
+
+        if (!CollegeData) return res.status(404).send({ status: false, message: "No such College is present" })
+
+        data.collegeId = CollegeData._id
 
         const createIntenData = await InternModel.create(data)
 
-        res.status(201).send({ status: true, newdata: createIntenData })
+        res.status(201).send({ status: true, newData: createIntenData })
+
     } catch (err) {
         res.status(500).send({ status: false, error: err.message })
     }
